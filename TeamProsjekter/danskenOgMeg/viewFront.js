e2194776d214809deb585d19baf7fcb680dd80e4
kotta96 <94447790+kotta96@users.noreply.github.com>
2022-03-17 11:20:49 +0100

    a

--- TeamProsjekter/danskenOgMeg/viewFront.js
+++ TeamProsjekter/danskenOgMeg/viewFront.js
@@ -1,8 +1,8 @@
 function updateViewFront() {
     html = `
-    ${horefaenhelvettedetteernoeheltjævligpiss()}
+    ${Header()}
     <div class="col-1"> ${apekatt}</div>
-    <div class="col-2">Hei</div>
+    <div class="col-2">Hei du</div>
     <div class="col-3">Logg inn</div>
     <div class="col-4" onclick="model.app.currentPage = 'Cart';updateView()">Handlevogn</div>
     <button class="col-3> Cart </button>
@@ -11,49 +11,3 @@ function updateViewFront() {
 return html;
 
 }
-function horefaenhelvettedetteernoeheltjævligpiss() {
-   return `
-        <h1 class="header">Hore faen potet Vintage Skatter.</h1>
-        `
-    //du har gått over din idiot..
-}
-
-// <li onclick="visStue(${model.fuckLister1})" class="dropdown"> ${model.categories[i].name}</li>
-            // ${model.fuckLister1}
-            // <li onclick="visStue(${i})" class="dropdown">${model.categories[i].name}</li>
-            // ${model.fuckLister2}
-            // <li onclick="visStue(${i})" class="dropdown">${model.categories[i].name}</li>
-            // ${model.fuckLister3}
-            // <li onclick="visStue(${i})" class="dropdown">${model.categories[i].name}</li>
-            // ${model.fuckLister4}
-
-
-
-    // for(let i = 0; i < model.products.length; i++){
-
-    //     if(model.products[i].categoryId == model.selectedCategory ){
-    //         html += model.products[i].title + "text her " + model.products[i].price
-    //         html += `<img src="${model.products[i].img}/>`; 
-    //     }
-    // }
-        
-    // for(let i = 0; i < model.categories[i].id; i++){
-
-    //     if(model.categories[i].id == model.categories[i].id ){
-    //         html += /*html*/ 
-    //         `   <li onclick="visStue(model.catagories[i].id)" class="dropdown">${model.categories[i].id}Stue</li>
-    //             <li onclick="visStue(model.catagories[i].id)" class="dropdown">Spiserom</li>
-    //             <li onclick="visStue(model.catagories[i].id)" class="dropdown">Kjøkken</li>
-    //             <li onclick="visStue(model.catagories[i].id)" class="dropdown">Soverom</li>
-    //             <li onclick="visStue(model.catagories[i].id)" class="dropdown">Bad</li>
-    //             <li onclick="visStue(model.catagories[i].id)" class="dropdown">Gang</li>
-    //             <li onclick="visStue(model.catagories[i].id)" class="dropdown">Klær</li>
-    //             <li onclick="visStue(model.catagories[i].id)" class="dropdown">Sko</li>
-
-            
-            
-    //         `
-    //       //Stopper her   
-    //     }
-    // }
-           
