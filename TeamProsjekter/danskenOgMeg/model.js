98d1ecc55fabaa3d9ee958d24acb629f1e2eda36
Scarecrow2905 <tommy-sc@live.no>
2022-03-30 11:31:08 +0200

    Update (Pay)

--- TeamProsjekter/danskenOgMeg/model.js
+++ TeamProsjekter/danskenOgMeg/model.js
@@ -51,16 +51,17 @@ const model = {
 
     // Navn på de forskjellige sider.
     app: {
-        currentPage: 'FrontPage', //FrontPage |  Product | LoginMail |LoginPhone | Cart | PaymentPage | 
+        currentPage: 'PayPage', //FrontPage |  Product | LoginMail |LoginPhone | Cart | PayPage | 
         currentUser: '',
     },
     //Inputs -!-
     idIndex: 1,
     //Inputs for login og å lage nye brukere
     account: {
-        users:[{firstName:'admin', lastName:'nordmann', phone: 0, email:'test@test.no', password:'abc123', id: 0  },
-        {firstName:'kåre', lastName:'nordmann', phone: 0, email:'fest@test.no', password:'abcd123', id: 0  },
-        {firstName: 'håre', lastName: 'nordmann', phone: 0, email: 'hest@test.no', password: 'abce123', id: 0 },
+        users: [
+            { firstName: 'admin', lastName: 'nordmann', phone: 0, email: 'test@test.no', password: 'abc123', id: 0 },
+            {firstName:'kåre', lastName:'nordmann', phone: 0, email:'fest@test.no', password:'abcd123', id: 0  },
+            {firstName: 'håre', lastName: 'nordmann', phone: 0, email: 'hest@test.no', password: 'abce123', id: 0 },
     ],
         // firstName:[],
         // lastName:[],
@@ -82,7 +83,12 @@ const model = {
     createCategory:[ 
         {name: '', id:'', sub:[]},
         {name: '', id:'', parentId: ''},
-        ],
+    ],
+        createSubCategory:[ 
+        {categoriName: '', id:'', parentId: ''},
+        
+    ],
+
     //Legge til nye produkter
     createItems: {
             title: '',
@@ -104,44 +110,101 @@ const model = {
     //OBS ::: Legge til False per kategori/Sub 
     ,
     categories:[
-        {name: 'Stue', id: 1, sub: [{ categoriName: 'Sofa', id: 11, parentId: 1 }, { categoriName: 'Skjenk', id: 12, parentId: 1 },] },
-        {name: 'Spiserom', id:2, sub:[{categoriName: 'Bord', id:21, parentId:2},  ]},
-        {name: 'Kjøkken', id:3, sub:[{categoriName: 'Stol', id:31, parentId:3},   ]},
-        {name: 'Soverom', id: 4, sub:[{categoriName: 'Seng', id:41, parentId:4},  ]},
-        {name: 'Bad', id:5, sub:[ {categoriName: 'Badekar', id:51, parentId:5},   ]},
-        {name: 'Gang', id:6, sub:[ {categoriName: 'Garderobe', id:61, parentId:6},]},
-        {name: 'Klær', id:7, sub:[ {categoriName: 'Kjole', id:71, parentId:7},    ]},
-        {name: 'Sko', id:8, sub:[{categoriName: 'Støvler', id:81, parentId:8},    ]},
+        {name: 'Stue', id: 1, 
+            sub: [
+                { categoriName: 'Sofa', id: 11, parentId: 1 },
+                { categoriName: 'Sofabord', id: 12, parentId: 1 },
+                { categoriName: 'Sjeselong', id: 14, parentId: 1 },
+                { categoriName: 'Kommoder', id: 15, parentId: 1 }, 
+                { categoriName: 'Hyller', id: 16, parentId: 1 },
+                { categoriName: 'Reol', id: 17, parentId: 1 },
+                { categoriName: 'Pyntegjenstander', id: 18, parentId: 1 },
+                { categoriName: 'Tekstil', id: 19, parentId: 1 },
+            ]
+        },
+        {
+            name: 'Spiserom', id: 2,
+            sub: [
+                { categoriName: 'Spisebord', id: 21, parentId: 2 },
+                { categoriName: 'Spisestuestoler', id: 22, parentId: 2 },
+                { categoriName: 'Reol-ER', id: 23, parentId: 2 },
+                { categoriName: 'Kommode', id: 24, parentId: 2 },
+                { categoriName: 'Skjenk', id: 25, parentId: 2 },
+                { categoriName: 'Kommode', id: 26, parentId: 2 },
+                { categoriName: 'Tekstil', id: 27, parentId: 2 },
+
+            ]
+        },
+        {
+            name: 'Kjøkken', id: 3, sub: [
+                { categoriName: 'Kjøkkenbord', id: 31, parentId: 3 },
+                { categoriName: 'Kjøkkenstoler', id: 32, parentId: 3 },
+                { categoriName: 'Hyller', id: 33, parentId: 3 },
+                { categoriName: 'Pyntegjenstander', id: 35, parentId: 3 },
+                { categoriName: 'Tekstil', id: 36, parentId: 3 },
+            ]
+        },
+        {
+            name: 'Soverom', id: 4, sub: [
+                { categoriName: 'Seng', id: 41, parentId: 4 },
+                { categoriName: 'Nattbord', id: 42, parentId: 4 },
+                { categoriName: 'Kommoder', id: 43, parentId: 4 },
+                { categoriName: 'Garderobeskap', id: 44, parentId: 4 },
+                { categoriName: 'Pyntegjenstander', id: 45, parentId: 4 },
+                { categoriName: 'Tekstil', id: 46, parentId: 4 },
+            ]
+        },
+        {
+            name: 'Bad', id: 5, sub: [
+                { categoriName: 'Badekar', id: 51, parentId: 5 },
+                { categoriName: 'Vaskebrett', id: 52, parentId: 5 },
+                { categoriName: 'Baderomsinnredning', id: 53, parentId: 5 },
+                { categoriName: 'Pyntegjenstander', id: 54, parentId: 5 },
+                { categoriName: 'Tekstil', id: 55, parentId: 5 },
+            ]
+        },
+        {
+            name: 'Gang', id: 6, sub: [
+                { categoriName: 'Sko oppbevaring', id: 61, parentId: 6 },
+                { categoriName: 'Garderobeskap', id: 62, parentId: 6 },
+                { categoriName: 'Hattehyller', id: 63, parentId: 6 },
+                { categoriName: 'Pyntegjenstander', id: 64, parentId: 6 },
+                { categoriName: 'Tekstil', id: 65, parentId: 6 },
+            ]
+        },
+        {
+            name: 'Klær', id: 7, sub: [
+                { categoriName: 'Kjole', id: 71, parentId: 7 },
+                { categoriName: 'Bukser', id: 72, parentId: 7 },
+                { categoriName: 'Gensere', id: 73, parentId: 7 },
+                { categoriName: 'Jakker', id: 74, parentId: 7 },
+                { categoriName: 'Cardigans', id: 75, parentId: 7 },
+                { categoriName: 'Hatter', id: 76, parentId: 7 },
+                { categoriName: 'Skjerf', id: 77, parentId: 7 },
+                { categoriName: 'Øredobber', id: 78, parentId: 7 },
+            ]
+        },
+        {
+            name: 'Sko', id: 8, sub: [
+                { categoriName: 'Støvletter', id: 81, parentId: 8 },
+                { categoriName: 'Støvler', id: 82, parentId: 8 },
+                { categoriName: 'Joggesko', id: 83, parentId: 8 },
+                { categoriName: 'Platåsko', id: 84, parentId: 8 },
+                { categoriName: 'Høyhælte sko', id: 85, parentId: 8 },
+            ]
+        },
+        {
+            name: 'Lesesal', id: 9, sub: [
+                { categoriName: 'Godstol', id: 91, parentId: 9 },
+                { categoriName: 'Bokhyller', id: 92, parentId: 9 },
+                { categoriName: 'Avlastningsbord', id: 93, parentId: 9 },
+                { categoriName: 'Pyntegjenstander', id: 94, parentId: 9 },
+                { categoriName: 'Tekstil', id: 95, parentId: 9 },
+            ]
+        },
     ],
    
-    // categories:[
-    //     {name:'Stue', id:1},
-    //     {name: 'Spiserom', id:2},
-    //     {name: 'Kjøkken', id:3},
-    //     {name: 'Soverom', id: 4},
-    //     {name: 'Bad', id:5},
-    //     {name: 'Gang', id:6},
-    //     {name: 'Klær', id:7},
-    //     {name: 'Sko', id:8},
-    // ],
-    // categoriesSub:[
-    //     {categoriName:'Sofa', id:11,parentId:1},
-    //     {categoriName:'Skjenk', id:12,parentId:1},
-
-    //     {categoriName: 'Bord', id:21, parentId:2},
-
-    //     {categoriName: 'Stol', id:31, parentId:3},
-
-    //     {categoriName: 'Seng', id:41, parentId:4},
-
-    //     {categoriName: 'Badekar', id:51, parentId:5},
-
-    //     {categoriName: 'Garderobe', id:61, parentId:6},
-
-    //     {categoriName: 'Kjole', id:71, parentId:7},
-        
-    //     {categoriName: 'Støvler', id:81, parentId:8},   
-    // ],
+ 
     // Data -!-
 
     //Betalingsløsninger
@@ -154,36 +217,43 @@ const model = {
     // Admin og nye brukere
     members: [{
         username: 'Elin',
-        password:'monkeyBusiness',
-    }],
+        password: 'monkeyBusiness',
+        userlevel: 'admin'
+    },
+    {
+        username: 'fake',
+        password: 'gay',
+        userlevel: 'user'
+    },
+    ],
     
     //Alle produkter
     products:[
         
         // Stue
         {
-            title: 'hore',
-            price: 6000,
-            stock: 69,
+            title: 'Sofa',
+            price: 8999,
+            stock: 2,
             category: 'Stue',
             categoryId: 11,
-            subCategory: 'Store ting',
-            img: 'testSofa.jpg',
+            subCategory: 'Sofa',
+            img: '/testBilder/vintagesofaoker.jpeg',
             description: 'Godt brukt, men fortsatt en flott sofa. Stått lagret i bod siden 2005.',
-            measures: 'Høyde 80cm. Dybde 50cm.',
+            measures: 'Høyde: 80cm. Dybde: 50cm. Lengde: 170cm',
             color: ['Oker Gul'],
             country: 'Danmark',
             year: '2006',
         },
 
         {
-            title: 'Vintage Sofa',
-            price: 11000,
-            stock:54,
-            category: 'Stue',
+            title: 'Spisestuestol',
+            price: 3499,
+            stock:4,
+            category: 'Spiserom',
             categoryId: 12,
-            subCategory: 'Store ting',
-            img: 'testSofa.jpg',
+            subCategory: 'Spisestuestol',
+            img: '',
             description: 'Sofa med 3 seter. Litt brukt, pent ivaretatt.',
             measures: 'Høyde 90cm. Dybde 89cm. Lengde 240cm.',
             color: ['Mørk grønn', ' Beige'],
@@ -192,13 +262,13 @@ const model = {
         },  
         // Spiserom
         {
-            title: 'Sofa Bård',
-            price: 8700,
-            stock: 12,
-            category: 'Stue',
+            title: 'Kjøkkenbord',
+            price: 6699,
+            stock: 1,
+            category: 'Kjøkken',
             categoryId: 21,
-            subCategory: 'Store ting',
-            img: 'testSofa.jpg',
+            subCategory: 'Kjøkkenbord',
+            img: '',
             description: 'Pent brukt, godt lagret.',
             color: ['Mørk grønn'],
             country: 'Italia',
@@ -207,26 +277,26 @@ const model = {
             
         // Kjøkken
         {   
-            title: 'biff gardiner',
-            price: 3000,
-            stock: 5,
-            category: 'Kjøkken',
+            title: 'Seng',
+            price: 7899,
+            stock: 2,
+            category: 'Soverom',
             categoryId: 32,
-            subCategory: '',
-            img: 'testSofa.jpg',
+            subCategory: 'Seng',
+            img: '/testBilder/80svintagebord.jpeg',
             description: '',
             color: ['metall', 'svart'],
             country: 'Danmark',
             year: '1971',
         }, 
         {
-            title: 'bortskjemt drittunge',
-            price: 1,
-            stock: 1,
-            category: 'Stue',
-            categoryId: 1781,
-            subCategory: 'Store ting',
-            img: 'testSofa.jpg',
+            title: 'Badekar',
+            price: 9799,
+            stock: 3,
+            category: 'Bad',
+            categoryId: 51,
+            subCategory: 'Badekar',
+            img: '',
             description: 'Godt brukt, men fortsatt en flott sofa. Stått lagret i bod siden 2005.',
             measures: 'Høyde 80cm. Dybde 50cm.',
             color: ['Oker Gul'],
@@ -234,18 +304,60 @@ const model = {
             year: '2017',
         },
         {
-            title: 'Hobo´s Dildo',
-            price: 6000,
-            stock: 3,
-            category: 'Stue',
-            categoryId: 99,
-            subCategory: 'Store ting',
-            img: 'testSofa.jpg',
+            title: 'Garderobeskap',
+            price: 8899,
+            stock: 2,
+            category: 'Gang',
+            categoryId: 62,
+            subCategory: 'Gardeobeskap',
+            img: '',
             description: 'Godt brukt, men fortsatt en flott sofa. Stått lagret i bod siden 2005.',
             measures: 'Høyde 80cm. Dybde 50cm.',
             color: ['Oker Gul'],
             country: 'Danmark',
             year: '2006',
+        },
+        {
+            title: 'Kjole',
+            price: 2999,
+            stock: 1,
+            category: 'Klær',
+            categoryId: 71,
+            subCategory: 'Kjoler',
+            img: '',
+            description: 'Godt brukt, med fortsatt mye bruk igjen. Stått lagret i bod siden 2005.',
+            measures: 'Lengde 39cm. Bredde 7cm.',
+            color: ['Red', 'blue','green'],
+            country: 'uganda',
+            year: '1500~',
+        },
+        {
+            title: 'Støvletter',
+            price: 12500,
+            stock: 1,
+            category: 'Sko',
+            categoryId: 101,
+            subCategory: 'Støvletter',
+            img: '',
+            description: 'brukt et par ganger',
+            measures: 'Lengde 2cm. Bredde 3cm.',
+            color: ['white', 'grey','purple'],
+            country: 'Denmark',
+            year: '1990~',
+        },
+        {
+            title: ' Globus 1',
+            price: 4999,
+            stock: 2,
+            category: 'Lesesal',
+            categoryId: 102,
+            subCategory: '',
+            img: '',
+            description: 'Hele verden rundt siden 1880',
+            measures: 'Høyde: 50cm ',
+            color: ['black', 'brown','tan'],
+            country: 'Denmark',
+            year: '1880ca',
         },
     ],
 }
--- TeamProsjekter/danskenOgMeg/view/viewPay.js
+++ TeamProsjekter/danskenOgMeg/view/viewPay.js
@@ -2,9 +2,80 @@ function updateViewPay(){
 html = `
     <div class="pay-main">
         <div class="pay-title"> 
+        <button class="btn-cart-navigation" onclick="model.app.currentPage = 'FrontPage';updateView()">Tilbake</button>
+        <div> Total ${model.shoppingCart.totalPrice},- 
+        </div>
+
+        <div class="pay-checkbox"> 
+        <label class="container"> Kort
+            <input id="card" type="checkbox" checked="checked">
+            <span class="checkmark"></span>
+        </label>
+
+        <label class="container"> PayPal
+            <input id="PayPal" type="checkbox">
+            <span class="checkmark"></span>
+        </label>
+
+        <label class="container"> Vipps
+            <input type="checkbox">
+            <span class="checkmark"></span>
+        </label>
+        </div>
+
+        <div class="pay-form">
+
+        <form action="receipt.php" method="post" id="the-form">
+        <div class="input input-text half-width">
+        <span class="input-label">Fornavn</span>
+        <input class="input-field" type="text" placeholder="Jon" name="fname" autofocus>
+        </div>
+
+        <div class="input" input-text half-width">
+        <span class="input-label">Etternavn</span>
+        <input class="input-field" type="text" placeholder="Brun" name="lname">
+        </div>
+
+        <div class="input" input-text full width">
+        <span class="input-label>E-post adresse</span>
+        <input class="input-field" type="text" placeholder="someone@example.com" name="email">
+        </div>
+
+        <div class="input input-text full-width">
+        <span class="input-label">Kortnummer</span>
+        <input class="input-field" type="text" placeholder="0000 0000 0000 0000">
+        <div id="ccnumber"></div>
+        </div>
+
+        <div class="input input-text half-width">
+        <span class="input-label">Utløpsdato</span>
+        <input class="input-field" type="numbers" placeholder="10 / 12">
+        <div id="ccexp"></div>
+        </div>
+
+        <div class="input input-text half-width">
+        <span class="input-label">CVV</span>
+        <input class="input-field" type"numbers" placeholder="123">
+        <div id="cvv"></div>
+
+        
+        </div>
+        
+        
+        <button class="btn-buy" onclick="purchase()"> Kjøp</button>
+
+
+
+
     </div>
 
+    
+
 `
+
+
+
+
 
 return html;
 };
@@ -12,71 +83,71 @@ return html;
 
 /*<head>
 <title>Betalingsdemo</title>
-<link rel=«stylesheet» href=«style.css»>
+<link rel="stylesheet" href="style.css">
 <script
-src=«https://secure.transactiongateway.com/token/collect.js»
-data-tokenization-key=«your-key-goes-here»
-data-variant=«inline»
+src="https://secure.transactiongateway.com/token/collect.js"
+data-tokenization-key="your-key-goes-here"
+data-variant="inline"
 data-field-ccnumber-placeholder = ’0000 0000 0000 0000’
 data-field-ccexp-placeholder = ’10 / 22’
 data-field-cvv-placeholder = ‘123’
 data-custom-css=‘{
-«border-style» : «solid»,
-«border-color» : «c7c7c7»,
-«border-width» : «1px»,
-«border-radius» : «3px»,
-«padding» : «6px»,
-«font-size» : «16px»,
-«height» : «33px»
+"border-style" : "solid",
+"border-color" : "c7c7c7",
+"border-width" : "1px",
+"border-radius" : "3px",
+"padding" : "6px",
+"font-size" : "16px",
+"height" : "33px"
 }’
 
 </script>
 
 </head>
 
-<div class=«nav»>
-<div class=«nav-item» ><a href=«#»>Store</a></div>
-<div class=«nav-item active» aria-current=«page»>Checkout</div>
+<div class="nav">
+<div class="nav-item" ><a href="#">Store</a></div>
+<div class="nav-item active" aria-current="page">Checkout</div>
 </div>
 
-<div class=«container»>
+<div class="container">
 <h1>
 <span>Checkout</span>
 <span>$9.99</span>
 </h1>
 
-<form action=«receipt.php» method=«post» id=«the-form»>
-<div class=«input input-text half-width»>
-<span class=«input-label»>Fornavn</span>
-<input class=«input-field» type=«text» placeholder=«John» name=«fname» autofocus>
+<form action="receipt.php" method="post" id="the-form">
+<div class="input input-text half-width">
+<span class="input-label">Fornavn</span>
+<input class="input-field" type="text" placeholder="John" name="fname" autofocus>
 </div>
 
-<div class=«input» input-text half-width»>
-<span class=«input-label»>Etternavn</span>
-<input class=«input-field» type=«text» placeholder=«Smith» name=«lname»>
+<div class="input" input-text half-width">
+<span class="input-label">Etternavn</span>
+<input class="input-field" type="text" placeholder="Smith" name="lname">
 </div>
 
-<div class=«input» input-text full width»>
-<span class=«input-label>E-post adresse</span>
-<input class=«input-field» type=«text» placeholder=«someone@example.com» name=«email»>
+<div class="input" input-text full width">
+<span class="input-label>E-post adresse</span>
+<input class="input-field" type="text" placeholder="someone@example.com" name="email">
 </div>
 
-<div class=«input input-text full-width»>
-<span class=«input-label»>Kortnummer</span>
-<div id=«ccnumber»></div>
+<div class="input input-text full-width">
+<span class="input-label">Kortnummer</span>
+<div id="ccnumber"></div>
 </div>
 
-<div class=«input input-text half-width»>
-<span class=«input-label»>Utløpsdato</span>
-<div id=«ccexp»></div>
+<div class="input input-text half-width">
+<span class="input-label">Utløpsdato</span>
+<div id="ccexp"></div>
 </div>
 
-<div class=«input input-text half-width»>
-<span class=«input-label»>CVV</span>
-<div id=«cvv»></div>
+<div class="input input-text half-width">
+<span class="input-label">CVV</span>
+<div id="cvv"></div>
 </div>
 
-<input class=«btn btn-mega» id=«payButton» type=«submit» value=«Submit Payment»>
+<input class="btn btn-mega" id="payButton" type="submit" value="Submit Payment">
 </form>
 </div>
 
@@ -90,13 +161,13 @@ $email = $_POST[‘email’];
 $payment_token = $_POST[‘payment_token’];
 
 $url = ‘https://secure.transactiongateway.com/api/transact.php';
-$vars = «security_key=4KTHldjfkgkdlfjggldfjgk»
-. «&type=sale»
-. «&amount = 9.99»
-. «&first_name=» . $fname
-. «&last_name=» . $fname
-. «&email=» . $email
-. «&payment_token=» . $payment_token;
+$vars = "security_key=4KTHldjfkgkdlfjggldfjgk"
+. "&type=sale"
+. "&amount = 9.99"
+. "&first_name=" . $fname
+. "&last_name=" . $fname
+. "&email=" . $email
+. "&payment_token=" . $payment_token;
 
 $ch = curl_init( $url );
 curl_setopt($ch, CURLOPT_POST, 1);
