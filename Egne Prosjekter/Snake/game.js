4e35d5b7f47f9e74c39066395ac2b298d8fa3015
Scarecrow2905 <tommy-sc@live.no>
2022-03-30 09:22:58 +0200

    Update (Eget prosjekt)

--- Egne Prosjekter/Snake/game.js
+++ Egne Prosjekter/Snake/game.js
@@ -1,29 +1,41 @@
 import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
-
+import { update as updateFood, draw as drawFood } from './food.js'
 
 let lastRenderTime = 0
+let gameOver = false
 const gameBoard = document.getElementById('game-board')
 
 
 function main(currentTime) {
+    if (gameOver) {
+        return alert('Game Over, try again')
+    }
+
+
     window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
 
-
     lastRenderTime = currentTime
 
-    update()
-    draw()
+    update() // Dette vil oppdatere all logikken i spillet
+    draw()  // Dette vil tegne opp spillet
 }
 
 window.requestAnimationFrame(main)
 
 function update() {
     updateSnake()
+    updateFood()
+    checkDeath()
 }
 
 function draw() {
-    gameBoard.innerHTML = '' // Dette hinder at slangen beholder lengden og ikke tar over hele brettet til slutt
+    gameBoard.innerHTML = '' // Dette hindrer at slangen beholder lengden og ikke tar over hele brettet til slutt
     drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
