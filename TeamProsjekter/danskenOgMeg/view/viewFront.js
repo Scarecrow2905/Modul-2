738c33dc46030805538b59afa4500dca4bf01f54
Thorbjørn <thorlox94@gmail.com>
2022-03-31 15:01:36 +0200

    added search icon
    Search Icon added
    
    obs Icon has the search function, (I think) it only requires search function to be coded.

--- TeamProsjekter/danskenOgMeg/view/viewFront.js
+++ TeamProsjekter/danskenOgMeg/view/viewFront.js
@@ -9,6 +9,12 @@ function updateViewFront() {
         <div class="liteFelt">
             <ul>
                 <li class="liteFeltLi">${searchfield()}</li>
+                <button class="search-icon" onclick="searchfield();"><i class="fas fa-search"></i></button>
+               
+
+
+
+
                 <li class="liteFeltLi"><button onclick="model.app.currentPage = 'Cart';updateView()">Handlevogn ${model.shoppingCart.cartProducts.length} Pris: ${model.shoppingCart.totalPrice}</button></li>
                 <li class="liteFeltLi"><button onclick="checkLoginStatus();updateView();">${model.app.showStatus}</button></li> 
             <ul>
