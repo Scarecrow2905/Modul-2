899040606f47f4540e7950f56efc93d931b47983
Thorbjørn <thorlox94@gmail.com>
2022-04-04 13:26:36 +0200

    Font.fix
    La til fontene i Prosjekt mappa

--- TeamProsjekter/danskenOgMeg/view/viewProduct.js
+++ TeamProsjekter/danskenOgMeg/view/viewProduct.js
@@ -1,12 +1,21 @@
-function viewProduct () {
-    html = `
-    
-    `
+// function viewProduct() {
+//     for (let i = 0; i < model.products.length; i++) {
+//         let html = '';
+//         model.app.currentPage = 'LoginMail'
+//         html += /*html*/`
+//         </div>
+//         < class="produkt">
+//         <div>   ${model.products[i].title}</div>
+//           <div>  ${model.products[i].price}</div>
+//           <div>  ${model.products[i].stock}</div>
+//         <img class="" src="${model.products[i].img}"/>
+//         <p>morra puller fungerer du nå?</p>
 
-    return html;
-}
+//   `
 
+//         return html;
+//     };
+//     console.log(p)
+// }
 
-
-
-
+// Funker ikke bare slett
