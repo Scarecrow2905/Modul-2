28f2da8b09bd17d5f6c36f2f035f43bdc27d3b7a
Tommy <tommy-sc@live.no>
2022-04-07 14:47:21 +0200

    Update (viewProduct)

--- TeamProsjekter/danskenOgMeg/view/viewProduct.js
+++ TeamProsjekter/danskenOgMeg/view/viewProduct.js
@@ -29,7 +29,7 @@ function viewProductItem() {
     for (let i = 0; i < model.products.length; i++)
     if(model.products[i].id == model.modalPopupInformation)
     result = `<section class="showProduct">
-            <h2 class ="product-header"> ${model.products[i].title}</h2>
+            <div class="product-header"> ${model.products[i].title}</div>
             <div class="product-row">
 
                 <div class="product-item product-column">
@@ -37,7 +37,13 @@ function viewProductItem() {
                 </div>
 
                 <div class="product-item">
-                <span class="product-item-description">${model.products[i].description}"</span>
+                    <span class="product-item-description">
+                            ${model.products[i].description} 
+                    Mål:    ${model.products[i].measures}
+                    Land:   ${model.products[i].country}
+                    Farge:  ${model.products[i].color}
+                    År:     ${model.products[i].year}
+                    </span>
                 </div>
     
             </div>
