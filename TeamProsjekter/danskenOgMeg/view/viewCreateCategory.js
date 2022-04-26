84ee8338fcc6a96070025c887a7075aaea8591fd
Thorbjørn <thorlox94@gmail.com>
2022-04-26 11:16:42 +0200

    Du kan bli

--- TeamProsjekter/danskenOgMeg/view/viewCreateCategory.js
+++ TeamProsjekter/danskenOgMeg/view/viewCreateCategory.js
@@ -6,14 +6,24 @@ function viewCreateCategory() {
         html += /*html*/
             `
         <input class="" id="" onchange="category.name = this.value" value="${category.name}" placeholder="Kategori Navn" type= "text">
-
+        <button onclick="idCountCategory();">mekk ny Categori</button>
         `
 
     }
     return html;
 }
 
-
+function idCountCategory() {
+    for (let i = 0; i < model.categories.length; i++) {
+        let = newCategory;
+        model.categories[i].id = newCategory;
+        
+        newCategory +1;
+        
+    }
+    
+    console.log(idCountCategory + newCategory)
+}
 
 
     // <input class="" id="" onchange="category.sub = this.value" value="${category.sub}" placeholder="Underkategori" type="text">
